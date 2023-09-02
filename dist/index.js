0d3234134a01035f5866d96c30fa73c8092bcfd8
troycaselli <swordandtrowel1689@gmail.com>
2023-09-02 13:45:34 -0700

    nullable types and optional operators

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,38 @@
 "use strict";
+var _a;
 let employee = {
-  id: 1,
-  name: "James",
-  retire: (date) => {
-    console.log(date);
-  },
+    id: 1,
+    name: "James",
+    retire: (date) => {
+        console.log(date);
+    },
 };
 console.log(employee);
-
 function kgToLbs(weight) {
-  if (typeof weight === "number") {
-    return weight * 2.2;
-  } else {
-    return parseInt(weight) * 2.2;
-  }
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
 }
-kgToLbs(10);
-kgToLbs("10kg");
+console.log(kgToLbs(10));
+console.log(kgToLbs("10kg"));
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 50;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola, friend!");
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,36 @@ let textBox: UIWidget = {
 // variable value is limited to specific values
 type Quantity = 50 | 100 | "50" | "100";
 let quantity: Quantity = 50;
+
+// NULLABLE TYPES
+function greet(name: string | null) {
+  if (name) console.log(name.toUpperCase());
+  else console.log("Hola, friend!");
+}
+
+greet(null);
+
+// OPTIONAL CHAINING
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+
+// if (customer !== null && customer !== undefined)
+// console.log(customer.birthday);
+
+// OPTIONAL PROPERTY ACCESS OPERATOR :::: Alternative to above two lines:
+console.log(customer?.birthday?.getFullYear());
+
+// OPTIONAL ELEMENT ACCESS OPERATOR
+// customers?.[0]
+
+// OPTIONAL CALL OPERATOR FOR FUNCTIONS
+let log: any = null;
+// question mark prevents crash because log is not = to a function.
+log?.("a");
